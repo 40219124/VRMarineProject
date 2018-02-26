fd5bde4a3b43e8cb849f7755b5bd2fb2a2879cac
Emma Parsley <40206111@live.napier.ac.uk>
2018-02-26 17:14:22 +0000

    Explanation -Commented code -added max height that can be set

--- Unity/MarineVR/Assets/Scripts/Procedural.cs
+++ Unity/MarineVR/Assets/Scripts/Procedural.cs
@@ -11,34 +11,46 @@ public class Procedural : MonoBehaviour {
     private GameObject coral;
     [SerializeField]
     private int objects = 100;
+    [SerializeField]
+    private int max_height = 0;
 
 	// Use this for initialization
 	void Start () {
+        //call generate to generate coral on load
         Generate();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //if P is pressed generate more *****FOR DEBUG******
 		if (Input.GetKey(KeyCode.P))
         {
             Generate();
         }
 	}
 
+    //method to generate sea life
     private void Generate()
     {
+        //loop for every object
         for (int i = 0; i < objects; i++)
         {
-            Vector3 pos = transform.localPosition + new Vector3(Random.Range(-width / 2, width / 2), 0, Random.Range(-length / 2, length / 2));
+            //choose random point
+            Vector3 pos = transform.localPosition + new Vector3(Random.Range(-width / 2, width / 2), max_height, Random.Range(-length / 2, length / 2));
+
+            //variables for raycasting
             Ray down = new Ray(pos, Vector3.down);
             RaycastHit hit;
 
+            //check if above ground
             if (Physics.Raycast(down, out hit))
             {
+                //if hits ground
                 if (hit.collider.tag == "Ground")
                 {
-                    Debug.Log("Ground");
+                    //set y position to hit point
                     pos.y = hit.point.y;
+                    //create coral
                     Instantiate(coral, pos, Quaternion.identity);
 
                 }
@@ -49,7 +61,7 @@ public class Procedural : MonoBehaviour {
 
     void OnDrawGizmosSelected()
     {
-        //draw range sphere
+        //draw cube showing affected area
         Gizmos.color = new Color(0.5f, 0.0f, 0.5f, 0.2f);
         Gizmos.DrawCube(transform.localPosition, new Vector3(width, 2, length));
     }
