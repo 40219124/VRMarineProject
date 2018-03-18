3dcdb41fd515327fc2947b8204d7ce4529cc444f
DESKTOP-LJH4P5G\drewj <40206862@live.napier.ac.uk>
2018-03-18 18:12:27 +0000

    Attempt at collision w/ fish

--- Unity/MarineVR/Assets/Scripts/PlayerBodyMovement.cs
+++ Unity/MarineVR/Assets/Scripts/PlayerBodyMovement.cs
@@ -7,26 +7,61 @@ public class PlayerBodyMovement : MonoBehaviour
 {
 
     private SteamVR_TrackedController[] controller = new SteamVR_TrackedController[2];
-    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_TrackedObject[] trackedObj = new SteamVR_TrackedObject[2];
     // device 0 = left, device 1 = right
     private SteamVR_Controller.Device[] device = new SteamVR_Controller.Device[2];
+    private Transform camTrans;
+    private float bearing = 0.0f;
+    private float deadZone = 7.0f;
+    private float ySpeed = 0.8f;
+    private float holdTime = 0.5f;
+    private float clock = 0.0f;
+    private bool turnBody = false;
 
     // Use this for initialization
     void Start()
     {
+        Transform[] transs = GetComponentsInChildren<Transform>();
+        foreach (Transform t in transs)
+        {
+            if (t.name.Contains("eye"))
+            {
+                camTrans = t;
+                break;
+            }
+        }
+        controller[0] = GetComponent<SteamVR_ControllerManager>().left.GetComponent<SteamVR_TrackedController>();
+        controller[1] = GetComponent<SteamVR_ControllerManager>().right.GetComponent<SteamVR_TrackedController>();
+        trackedObj[0] = controller[0].GetComponent<SteamVR_TrackedObject>();
+        trackedObj[1] = controller[1].GetComponent<SteamVR_TrackedObject>();
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (Time.time == Time.deltaTime)
+        device[0] = SteamVR_Controller.Input((int)trackedObj[0].index);
+        device[1] = SteamVR_Controller.Input((int)trackedObj[1].index);
+        
+        if (device[1].GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad) && !turnBody)
         {
-            controller[0] = GetComponent<SteamVR_ControllerManager>().left.GetComponent<SteamVR_TrackedController>();
-            controller[1] = GetComponent<SteamVR_ControllerManager>().right.GetComponent<SteamVR_TrackedController>();
-            trackedObj = controller[0].GetComponent<SteamVR_TrackedObject>();
+            turnBody = true;
         }
-        device[0] = SteamVR_Controller.Input((int)trackedObj.index);
-        device[1] = SteamVR_Controller.Input((int)controller[1].GetComponent<SteamVR_TrackedObject>().index);
+        else if (device[1].GetPressDown(EVRButtonId.k_EButton_SteamVR_Touchpad) && turnBody)
+        {
+            turnBody = false;
+            clock = 0.0f;
+        }
+        if (turnBody) { 
+            bearing = camTrans.eulerAngles.y;
+            clock += Time.deltaTime;
+        }
+        if (device[1].GetPressUp(EVRButtonId.k_EButton_SteamVR_Touchpad) && clock >= holdTime)
+        {
+            turnBody = false;
+            clock = 0.0f;
+        }
+        
+
         if (device[0].GetPress(EVRButtonId.k_EButton_SteamVR_Touchpad))
         {
             Vector2 touchpad = (device[0].GetAxis(EVRButtonId.k_EButton_Axis0));
@@ -35,19 +70,32 @@ public class PlayerBodyMovement : MonoBehaviour
             {
                 touchpad = touchpad.normalized;
             }
-            else if(sqLen > 0.0f)
+            else if (sqLen > 0.0f)
             {
                 touchpad = touchpad.normalized / 2.0f;
             }
-            transform.Translate(new Vector3(touchpad.x, 0.0f, touchpad.y) * Time.deltaTime);
+            transform.Translate(Quaternion.Euler(0.0f, bearing, 0.0f) * new Vector3(touchpad.x, 0.0f, touchpad.y) * Time.deltaTime);
         }
+
         if (device[0].GetPress(EVRButtonId.k_EButton_SteamVR_Trigger))
         {
-            transform.Translate(Vector3.down * 0.8f * Time.deltaTime);
+            transform.Translate(Vector3.up * ySpeed * Time.deltaTime);
+        }
+        if (device[0].GetPress(EVRButtonId.k_EButton_Grip))
+        {
+            transform.Translate(Vector3.down * ySpeed * Time.deltaTime);
         }
         if (device[1].GetPress(EVRButtonId.k_EButton_SteamVR_Trigger))
         {
-            transform.Translate(Vector3.up * 0.8f * Time.deltaTime);
+            float zRot = controller[1].GetComponent<Transform>().eulerAngles.z;
+            if (zRot > deadZone && zRot < 180.0f - deadZone)
+            {
+                transform.Translate(Vector3.down * ySpeed * Time.deltaTime);
+            }
+            else if (zRot > 180 + deadZone && zRot < 360.0f - deadZone)
+            {
+                transform.Translate(Vector3.up * ySpeed * Time.deltaTime);
+            }
         }
     }
 }
