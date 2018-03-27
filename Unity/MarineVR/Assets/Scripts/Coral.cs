649ea5cc2b30d0b107808a93f336fb82a8801a3f
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2018-03-27 18:23:18 +0100

    Rarity, Size, Colour and Clusters - Allowed you to change the rarity of objects - Size and colour is now slightly randomly changed - Objects can now spawn in clusters

--- Unity/MarineVR/Assets/Scripts/Coral.cs
+++ Unity/MarineVR/Assets/Scripts/Coral.cs
@@ -11,9 +11,11 @@ public class Coral : MonoBehaviour {
     public float steepest = 90.0f;
     public float shallowest = 0.0f;
     public float rarity = 20;
+    public int groupMin= 0;
+    public int groupMax= 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
