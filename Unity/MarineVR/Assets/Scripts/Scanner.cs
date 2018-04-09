a3c8d11e8ba1e4bfb0634d430168d56d864a8f4b
NAPIER-MAIL\40206862 <40206862@live.napier.ac.uk>
2018-04-09 17:12:43 +0100

    Canvas w/ info appears when colliding with objects. Moved everything from infoSys to scanner

--- Unity/MarineVR/Assets/Scripts/Scanner.cs
+++ Unity/MarineVR/Assets/Scripts/Scanner.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scanner : MonoBehaviour
 {
     public float reach;
     Transform parent;
     bool hasParent = false;
+    [SerializeField]
+    Text canvas;
 
     // Use this for initialization
     void Start()
     {
-
+        canvas = GameObject.Find("infoCanvas").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -29,5 +32,17 @@ public class Scanner : MonoBehaviour
         {
             gameObject.transform.position = parent.transform.position + parent.transform.rotation * (Vector3.forward * reach);
         }
+    }
+
+    //Should update if colliding with anything
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("collision detected");
+        canvas.text = Data.dictionaryData[collision.tag];
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        canvas.text = "";
     }
 } // /Model/tip/attach
