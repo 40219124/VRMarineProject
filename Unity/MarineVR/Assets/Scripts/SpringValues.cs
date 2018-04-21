1e4ccbceaabbc9543777ec3b370502da48e4ce5b
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2018-04-21 20:40:36 +0100

    Made it easier to test different spring values. Moved scripts to folder.

---
+++ Unity/MarineVR/Assets/Scripts/SpringValues.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringValues : MonoBehaviour
+{
+
+    public static float strength;
+    public static float damper;
+    public static int updateValues = 0;
+    private int myUpdates = 0;
+
+    public float Strength { set { strength = value; } }
+    public float Damper { set { damper = value; } }
+    public void UpdateValues() { updateValues += 1; }
+
+    private SpringJoint spring;
+
+    // Use this for initialization
+    void Start()
+    {
+        spring = gameObject.GetComponent<SpringJoint>();
+        spring.spring = strength;
+        spring.damper = damper;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myUpdates < updateValues)
+        {
+            spring.spring = strength;
+            spring.damper = damper;
+            myUpdates = updateValues;
+        }
+    }
+}
